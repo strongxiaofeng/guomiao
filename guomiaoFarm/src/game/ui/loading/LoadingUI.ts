57f3f83f1aeb7e8ada72a087f2eb317ec03fc210
tuxiaofengincompany <441664184@qq.com>
2017-11-17 13:12:35 +0800

    正常登陆

--- guomiaoFarm/src/game/ui/loading/LoadingUI.ts
+++ guomiaoFarm/src/game/ui/loading/LoadingUI.ts
@@ -13,8 +13,8 @@ class LoadingUI extends BaseUI{
 		super.initSetting();
 		UIManager.openUI(UIConst.TipErrorUI, LayerManager.Layer_Sys);
 		UIManager.openUI(UIConst.TipGreenUI, LayerManager.Layer_Sys);
-		// GameController.getInstance().getUserInfo(window["params"]); 
-		GameController.getInstance().getUserInfo("token=0c057f67d5827b6571e2764fe6e3bcf3"); 
+		GameController.getInstance().getUserInfo(window["params"]); 
+		// GameController.getInstance().getUserInfo("token=0c057f67d5827b6571e2764fe6e3bcf3"); 
 		GameController.getInstance().getServerConfig();
 
 		this.load();
